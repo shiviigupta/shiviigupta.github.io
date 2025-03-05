aafe29dbc76424cc923f994cf6b2e785ec8d392d
shiviigupta <47184100+shiviigupta@users.noreply.github.com>
2025-03-05 19:48:03 +0000

    Deploying to gh-pages from @ shiviigupta/shiviigupta.github.io@79082280e2a5eb1c7085425389059b375af2dc9a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -410,16 +410,11 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+    },{id: "news-mortar-board-b-tech-from-iit-kanpur-with-a-double-major-in-electrical-engineering-and-physics-minors-in-computer-science-and-english-literature",
+          title: ':mortar_board: B.Tech. from IIT Kanpur with a double major in Electrical Engineering and...',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          section: "News",},{id: "news-mortar-board-upcoming-ms-from-carnegie-mellon-university-with-a-major-in-electrical-engineering-concentration-in-ai-ml",
+          title: ':mortar_board: [Upcoming] MS from Carnegie Mellon University with a major in Electrical Engineering,...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
@@ -471,14 +466,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%73%68%69%76%69%67@%61%6E%64%72%65%77.%63%6D%75.%65%64%75", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/shiviigupta", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/shivigup", "_blank");
         },
       },{
         id: 'social-rss',
@@ -486,20 +488,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
